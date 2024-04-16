a9c0b0fc0a7d2da7483bf963ddc85ac69407bb1c
Fadi <fadishadow@hotmail.com>
2024-04-16 15:13:50 +0200

    Added Index to view name

--- Gupp8_MVC/Controllers/HomeController.cs
+++ Gupp8_MVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Gupp8_MVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
